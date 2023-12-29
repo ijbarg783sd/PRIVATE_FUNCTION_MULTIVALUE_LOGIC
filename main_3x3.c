f6913e24912602791b282ad4ff26ab75d4f59e50
dev <devtwitpwm01@proton.me>
2023-12-29 12:08:04 +0100

    adjusting pattern 10 (3.3 3.4 ok)

--- main_3x3.c
+++ main_3x3.c
@@ -431,33 +431,44 @@ int main() {
 //
 //                    ) {
 
-
                     // PATTERN 3.3 (x28 / 4 = x7)
-                    if(
-                            (bitCentrale != mtx[0][1] && bitCentrale != mtx[1][0] && bitCentrale != mtx[1][2] && bitCentrale != mtx[2][0] && bitCentrale != mtx[2][1]  && bitCentrale != mtx[2][2]) &&
+//                    if(
+//                            (bitCentrale != mtx[0][1] && bitCentrale != mtx[1][0] && bitCentrale != mtx[1][2] && bitCentrale != mtx[2][0] && bitCentrale != mtx[2][1]  && bitCentrale != mtx[2][2]) &&
+//
+//                            //"L" UNITA
+//                            !(mtx[2][1] == mtx[2][0] && mtx[2][0] == mtx[1][0] && mtx[1][0] == mtx[2][1]) &&
+//                            !(mtx[1][2] == mtx[2][2] && mtx[2][2] == mtx[2][1] && mtx[2][1] == mtx[1][2]) &&
+//
+//                            //"L" STACCATA
+//                            !(mtx[1][0] == mtx[2][0] && mtx[1][0] == mtx[1][2] && mtx[2][0] == mtx[1][2] && mtx[1][0] != mtx[2][2]) &&
+//                            !(mtx[1][0] == mtx[2][0] && mtx[1][0] == mtx[2][2] && mtx[2][0] == mtx[2][2] && mtx[1][0] != mtx[1][2]) &&
+//
+//                            !(mtx[1][2] == mtx[2][2] && mtx[1][2] == mtx[1][0] && mtx[2][2] == mtx[1][0] && mtx[1][2] != mtx[2][0]) &&
+//                            !(mtx[1][2] == mtx[2][2] && mtx[1][2] == mtx[2][0] && mtx[2][2] == mtx[2][0] && mtx[1][2] != mtx[1][0]) &&
+//
+//                            !(mtx[2][1] == mtx[2][2] && mtx[2][1] == mtx[0][1] && mtx[2][2] == mtx[0][1]) &&
+//                            !(mtx[2][0] == mtx[2][1] && mtx[2][0] == mtx[0][1] && mtx[2][1] == mtx[0][1])
+//
+//                    ) {
 
-                            //"L" UNITA
-                            !(mtx[2][1] == mtx[2][0] && mtx[2][0] == mtx[1][0] && mtx[1][0] == mtx[2][1]) &&
-                            !(mtx[1][2] == mtx[2][2] && mtx[2][2] == mtx[2][1] && mtx[2][1] == mtx[1][2]) &&
-
-                            //"L" STACCATA
-                            !(mtx[1][0] == mtx[2][0] && mtx[1][0] == mtx[1][2] && mtx[2][0] == mtx[1][2] && mtx[1][0] != mtx[2][2]) &&
-                            !(mtx[1][0] == mtx[2][0] && mtx[1][0] == mtx[2][2] && mtx[2][0] == mtx[2][2] && mtx[1][0] != mtx[1][2]) &&
-
-                            !(mtx[1][2] == mtx[2][2] && mtx[1][2] == mtx[1][0] && mtx[2][2] == mtx[1][0] && mtx[1][2] != mtx[2][0]) &&
-                            !(mtx[1][2] == mtx[2][2] && mtx[1][2] == mtx[2][0] && mtx[2][2] == mtx[2][0] && mtx[1][2] != mtx[1][0]) &&
-
-                            !(mtx[2][1] == mtx[2][2] && mtx[2][1] == mtx[0][1] && mtx[2][2] == mtx[0][1]) &&
-                            !(mtx[2][0] == mtx[2][1] && mtx[2][0] == mtx[0][1] && mtx[2][1] == mtx[0][1])
-
-                    ) {
-
-
-
-
-                    // PATTERN 3.4 (x)
-
-
+                    // PATTERN 3.4 (x20 / 2 = x10)
+//                    if(
+//                            (bitCentrale != mtx[0][0] && bitCentrale != mtx[0][1] && bitCentrale != mtx[1][0] && bitCentrale != mtx[1][2] && bitCentrale != mtx[2][1]  && bitCentrale != mtx[2][2]) &&
+//
+//                            //"L" UNITA
+//                            !(mtx[0][0] == mtx[0][1] && mtx[0][1] == mtx[1][0] && mtx[1][0] == mtx[0][0]) &&
+//                            !(mtx[2][2] == mtx[2][1] && mtx[2][1] == mtx[1][2] && mtx[1][2] == mtx[2][2]) &&
+//
+//                            //"L" STACCATA
+//                            !(mtx[0][0] == mtx[0][1] && mtx[0][0] == mtx[2][1] && mtx[0][1] == mtx[2][1]) &&
+//
+//                            !(mtx[1][2] == mtx[2][2] && mtx[1][2] == mtx[1][0] && mtx[2][2] == mtx[1][0]) &&
+//
+//                            !(mtx[2][1] == mtx[2][2] && mtx[2][1] == mtx[0][1] && mtx[2][2] == mtx[0][1]) &&
+//
+//                            !(mtx[0][0] == mtx[1][0] && mtx[0][0] == mtx[1][2] && mtx[1][0] == mtx[1][2])
+//
+//                    ) {
 
                     // PATTERN 4.1 (x28 / 4 = x7)
 //                    if(
