faa5d5efbf2b5383747e7cec1e026c6b815e3c34
dev <devtwitpwm01@proton.me>
2024-02-05 09:56:24 +0100

    indagatore funzioni (mirato su singola funzione )

--- main.c
+++ main.c
@@ -8,70 +8,35 @@
 int main() {
     clock_t begin = clock();
 
-    int abc[27][3] = {
-            {0,0,0},
-            {0,0,1},
-            {0,0,2},
-            {0,1,0},
-            {0,1,1},
-            {0,1,2},
-            {0,2,0},
-            {0,2,1},
-            {0,2,2},
-            {1,0,0},
-            {1,0,1},
-            {1,0,2},
-            {1,1,0},
-            {1,1,1},
-            {1,1,2},
-            {1,2,0},
-            {1,2,1},
-            {1,2,2},
-            {2,0,0},
-            {2,0,1},
-            {2,0,2},
-            {2,1,0},
-            {2,1,1},
-            {2,1,2},
-            {2,2,0},
-            {2,2,1},
-            {2,2,2},
-    };
-
     int res;
+    int f;
 
-//    for(int i = 0; i<=26; i++) {
-//        printf("a: %d | b: %d | c: %d\n", abc[i][0], abc[i][1], abc[i][2]);
-//        for(int x = 0; x <= 2; x++) {
-//            for(int y = 0; y <= 2; y++) {
-//                res = (abc[i][0]*x + abc[i][1]*y + abc[i][2]) % 3;
-////                printf("x:%d|y:%d\n", x, y);
-//                printf("%3d", res);
-//            }
-//            printf("\n");
-//        }
-//        printf("\n");
-//    }
+    //TEST SINGOLA FUNZIONE
+    for(int x = 0; x <= 2; x++) {
+        for(int y = 0; y <= 2; y++) {
 
-    for(int i = 0; i<=26; i++) {
-        printf("i: %d | a: %d | b: %d | c: %d\n", i, abc[i][0], abc[i][1], abc[i][2]);
-        for(int x = 0; x <= 2; x++) {
-            for(int y = 0; y <= 2; y++) {
-//                res = (abc[i][0]*x*x + abc[i][1]*y*y*y + abc[i][2]) % 3; //RISPETTO ADDIZIONE
-//                res = ((abc[i][0]*x + abc[i][1]*y + abc[i][2]) + (abc[i][0]*x + abc[i][1]*y + abc[i][2])) % 3; //RISPETTO ADDIZIONE
-//                res = ((abc[i][0]*x + abc[i][0]*x + abc[i][2])) % 3; //RISPETTO ADDIZIONE
-//                res = (x*(2-x)+y*(2-y)+x*y*(2-x)*(2-y)) % 3;
-                res = (x*(2-x)) % 3;
+//                f = x*(2-x)+y*(2-y)+x*y*(2-x)*(2-y);
+//                f = x*(2-x)+y*(2-y)+x*y*(2-x)*(2-y);
+//                f = x*y*(2-x);
+//                f = x*(2-x);
+                f = x*(1-x)+y*(1-y);
+//                f = (2*x*x + 2*y*y) + 2;
+//                f = (x*x) + (2*y*y);
+//                f = ((2*x) - (x*x)) + ((2*y) - (y*y)) + (x * y);
 
-//                res = (abc[i][0]*x*x*x * abc[i][1]*y*y*y * abc[i][2]) % 3; //RISPETTO ADDIZIONE
-//                printf("x:%d|y:%d\n", x, y);
+            res = f % 3;
+
+            if ( res == -2) {
+                printf("%3d", res + 3);
+            } else if ( res == -1) {
+                printf("%3d", res + 3);
+            } else {
                 printf("%3d", res);
             }
-            printf("\n");
+
         }
         printf("\n");
     }
-
 
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
